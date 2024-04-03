58536f98b5a3d7100296a40536e51472f1d5534c
poojagavhane16 <165903365+poojagavhane16@users.noreply.github.com>
2024-04-03 14:56:54 +0000

    coversion & Comparision

---
+++ 01_basics/03_conversion_Operation.js
@@ -0,0 +1,24 @@
+/*let score = "33"
+
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber));
+
+let score1 = "33abc"
+
+let valueInNumber1 = Number(score1)
+console.log(typeof(valueInNumber1));
+console.log(valueInNumber1);
+*/
+//********Operations ********************
+
+let val = 4
+let negVal = -val
+console.log(negVal);
+
+let str1 = "Hello"
+let str2 = " Hi"
+let str3 = str1 + str2
+console.log(str3);
+
