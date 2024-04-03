a10f44523a6cf5461f02b98421205514049bd289
poojagavhane16 <165903365+poojagavhane16@users.noreply.github.com>
2024-04-03 14:00:30 +0000

    Variables & Datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "pooja123@gmail.com"
+var accountPassword = "12345"
+accountCity = "Pune"
+let accountState;
+
+//accountId = 2
+accountEmail = "ermail@gmail.com"
+accountPassword = "334411"
+accountCity = "Jaipur"
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,11 @@
+"use strict";
+
+console.log(3+3)
+
+let name = "Pooja"
+let age = 15
+
+console.log(typeof "Pooja")
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefinedn);
