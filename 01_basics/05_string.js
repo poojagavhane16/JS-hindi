d5fc71ee5f89660513f215504ecec38f575ed1c5
poojagavhane16 <165903365+poojagavhane16@users.noreply.github.com>
2024-04-04 10:33:58 +0000

    memory allocation and string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,9 @@
+const name = "Pooja"
+const repoCount = 50
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const str1 = new String("Pooja")
+console.log(str1[0]);
+console.log(str1.__proto__);
+console.log(str1.length);
---
+++ 01_basics/memoryAllocation.js
@@ -0,0 +1,19 @@
+let myYoutubeName = "ABC"
+
+let anotherName = myYoutubeName
+anotherName = "chaiaurcode"
+
+console.log(myYoutubeName);
+console.log(anotherName);
+
+let userOne = {
+    email : "user@gmail.com",
+    upi : "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "user2@yahoo.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
